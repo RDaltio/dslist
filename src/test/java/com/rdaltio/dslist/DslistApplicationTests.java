f2b9ee5392b7cce3903fea1178c11cf922a56234
Rafael Duarte Daltio <m320000@alunos.ipb.pt>
2025-01-23 21:13:17 +0000

    Project created

---
+++ src/test/java/com/rdaltio/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdaltio.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
