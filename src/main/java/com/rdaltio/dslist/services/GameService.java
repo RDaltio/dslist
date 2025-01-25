3c935203c0db91d187a0f9bef9672b82d2a482cd
Rafael Duarte Daltio <m320000@alunos.ipb.pt>
2025-01-25 00:28:33 +0000

    GET games by list

--- src/main/java/com/rdaltio/dslist/services/GameService.java
+++ src/main/java/com/rdaltio/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.rdaltio.dslist.services;
 import com.rdaltio.dslist.dto.GameMinDTO;
 import com.rdaltio.dslist.entities.Game;
 import com.rdaltio.dslist.entities.GameDTO;
+import com.rdaltio.dslist.projections.GameMinProjection;
 import com.rdaltio.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,11 @@ public class GameService {
     public GameDTO findById(Long Id){
         Game result = gameRepository.findById(Id).get();
         return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(GameMinDTO::new).toList();
     }
 }
